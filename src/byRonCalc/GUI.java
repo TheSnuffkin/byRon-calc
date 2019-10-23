e6a33630c1816d33f6556d41673cf8d4f9550b10
Cade Snuffer <snufferc@mail.gvsu.edu>
2019-10-23 19:27:24 -0400

    v0.2 cade

--- src/byRonCalc/GUI.java
+++ src/byRonCalc/GUI.java
@@ -1,183 +1,288 @@
 package byRonCalc;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener{
-    private String expression;
+    private String userInput;
     private final JFrame frame;
     private final JPanel panel;
     private final JTextArea text;
-    private final JButton but[], butAdd, butMinus, butMultiply, butDivide,
-            butEqual, butCancel, butSquareRoot, butSquare, butOneDevidedBy,
-            butCos, butSin, butTan, butxpowerofy, butlog, butrate, butabs, butBinary;
-
-    private final String[] numVals = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    private GridBagConstraints gbc;
+    private final JButton but[], butSquare, butExp, butClear, butSqrt, butRt,
+        butLog, butLN, butDivide, butCos, butSin, butTan, butOpenParen, butCloseParen,
+        butDel, butPi, butExe, butAdd, butMinus, butMultiply, butDot, butEX, butVar, butGraph,
+        butUndo, butAns, butComma;
 
     calc backEnd;
 
     public GUI(){
         frame = new JFrame("Calculator PH");
         frame.setResizable(false);
-        panel = new JPanel(new FlowLayout());
-
-        text = new JTextArea(2, 25);
+        panel = new JPanel(new GridBagLayout());
+        gbc = new GridBagConstraints();
+        text = new JTextArea(4, 10);
         but = new JButton[10];
         for (int i = 0; i < 10; i++) {
             but[i] = new JButton(String.valueOf(i));
         }
 
-        butAdd = new JButton("+");
-        butMinus = new JButton("-");
+        butSquare = new JButton("^2");
+        butExp = new JButton("^");
+        butSqrt= new JButton("√");
+        butRt = new JButton("∛");
+        butEX = new JButton("e^x");
+        butLog = new JButton("Log");
+        butLN = new JButton("LN");
+        butSin = new JButton("Sin");
+        butCos = new JButton("Cos");
+        butTan = new JButton("Tan");
+        butOpenParen = new JButton("(");
+        butCloseParen = new JButton(")");
+        butVar = new JButton("X");
+        butGraph = new JButton("Graph");
+        butDel = new JButton("Del");
+        butClear = new JButton("Clear");
         butMultiply = new JButton("*");
         butDivide = new JButton("/");
-        butEqual = new JButton("=");
-        butSquareRoot = new JButton("√");
-        butSquare = new JButton("x*x");
-        butOneDevidedBy = new JButton("1/x");
-        butCos = new JButton("Cos");
-        butSin = new JButton("Sin");
-        butTan = new JButton("Tan");
-        butxpowerofy = new JButton("x^y");
-        butlog = new JButton("log10(x)");
-        butrate = new JButton("x%");
-        butabs = new JButton("abs(x)");
-
-        butCancel = new JButton("C");
-        butBinary = new JButton("Bin");
-
+        butAdd = new JButton("+");
+        butMinus = new JButton("-");
+        butDot = new JButton(".");
+        butExe = new JButton("EXE");
+        butPi = new JButton("π");
+        butUndo = new JButton("Undo");
+        butAns = new JButton("Answer");
+        butComma = new JButton(",");
         backEnd = new calc();
-        expression = "";
+        userInput = "";
     }
 
     public void initialize() {
         frame.setVisible(true);
-        frame.setSize(330, 300);
+        frame.setSize( 400, 700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
-
-        panel.add(text);
-
-        for (int i = 1; i < 10; i++) {
-            panel.add(but[i]);
+        panel.setBorder(new EmptyBorder(4, 4, 4, 4));
+        gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.BOTH; // fill cell
+        gbc.weightx = .5; //expand to fill container size
+        gbc.weighty = .5; //  "    "   "      "       "
+        addComponent(text, 0, 0, 5);
+        text.setFont(text.getFont().deriveFont(24f));
+        addComponent(butSquare, 0, 1, 1);
+        addComponent(butExp, 1, 1, 1);
+        addComponent(butSqrt, 2, 1, 1);
+        addComponent(butRt, 3, 1, 1);
+        addComponent(butEX, 4, 1, 1);
+        addComponent(butLog, 0, 2, 1);
+        addComponent(butLN, 1, 2, 1);
+        addComponent(butSin, 2,2, 1);
+        addComponent(butCos, 3, 2, 1);
+        addComponent(butTan, 4, 2, 1);
+        addComponent(butOpenParen, 0, 3, 1);
+        addComponent(butCloseParen,1,3,1);
+        addComponent(butVar, 2,3,1);
+        addComponent(butGraph, 3, 3, 2);
+        for(int i = 1; i < 4; i++){
+            addComponent(but[i], i-1, 4, 1);
             but[i].addActionListener(this);
         }
-        panel.add(but[0]);
+        addComponent(butDel, 3, 4, 1);
+        addComponent(butClear, 4,4,1);
+        for(int i = 4; i < 7; i++){
+            addComponent(but[i], i-4, 5, 1);
+            but[i].addActionListener(this);
+        }
+        addComponent(butMultiply, 3, 5, 1);
+        addComponent(butDivide, 4, 5, 1);
+        for(int i = 7; i < 10; i++){
+            addComponent(but[i], i-7, 6, 1);
+            but[i].addActionListener(this);
+        }
+        addComponent(butAdd, 3, 6, 1);
+        addComponent(butMinus, 4, 6, 1);
+        addComponent(but[0], 0, 7, 1);
+        addComponent(butDot, 1, 7, 1);
+        addComponent(butPi, 3, 7, 1);
+        addComponent(butExe, 4, 7, 1);
+        addComponent(butUndo, 0, 8, 1);
+        addComponent(butAns, 1, 8, 1);
+        addComponent(butComma,2,8,1);
+
         but[0].addActionListener(this);
-
-        panel.add(butAdd);
-        panel.add(butMinus);
-        panel.add(butMultiply);
-        panel.add(butDivide);
-        panel.add(butSquare);
-        panel.add(butSquareRoot);
-        panel.add(butOneDevidedBy);
-        panel.add(butCos);
-        panel.add(butSin);
-        panel.add(butTan);
-        panel.add(butxpowerofy);
-        panel.add(butlog);
-        panel.add(butrate);
-        panel.add(butabs);
-        panel.add(butabs);
-        panel.add(butBinary);
-
-        panel.add(butEqual);
-        panel.add(butCancel);
-
+        butComma.addActionListener(this);
+        butDot.addActionListener(this);
+        butEX.addActionListener(this);
         butAdd.addActionListener(this);
         butMinus.addActionListener(this);
         butMultiply.addActionListener(this);
         butDivide.addActionListener(this);
         butSquare.addActionListener(this);
-        butSquareRoot.addActionListener(this);
-        butOneDevidedBy.addActionListener(this);
+        butExe.addActionListener(this);
+        butExp.addActionListener(this);
         butCos.addActionListener(this);
         butSin.addActionListener(this);
         butTan.addActionListener(this);
-        butxpowerofy.addActionListener(this);
-        butlog.addActionListener(this);
-        butrate.addActionListener(this);
-        butabs.addActionListener(this);
-        butBinary.addActionListener(this);
-
-        butEqual.addActionListener(this);
-        butCancel.addActionListener(this);
+        butClear.addActionListener(this);
+        butSqrt.addActionListener(this);
+        butOpenParen.addActionListener(this);
+        butCloseParen.addActionListener(this);
+        butRt.addActionListener(this);
+        butDel.addActionListener(this);
+        butLog.addActionListener(this);
+        butLN.addActionListener(this);
+        butPi.addActionListener(this);
+        butUndo.addActionListener(this);
+        butAns.addActionListener(this);
     }
 
+    private final void addComponent(Component c, int gridx, int gridy, int gridwidth) {
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.gridwidth = gridwidth;
+        panel.add(c, gbc);
+    }
+
+    private String displayExpression(){
+        userInput = backEnd.getExpression();
+        if (userInput.contains("pi")) {
+            userInput = userInput.replaceAll("pi", "π");
+        }
+        if(userInput.contains("sqrt")){
+            userInput = userInput.replaceAll("sqrt", "√");
+        }
+        if(userInput.contains("cbrt")){
+            userInput = userInput.replaceAll("cbrt", "∛");
+        }
+        return userInput;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         final Object source = e.getSource();
 
         for (int i = 0; i < 10; i++) {
             if (source == but[i]) {
-                expression = expression + i;
+                backEnd.addToExpression(""+i);
+                text.setText(displayExpression());
                 return;
             }
         }
 
-        if (source == butAdd) {
-            expression = expression + "+";
-        }
-
-        if (source == butMinus) {
-            expression = expression + "-";
-        }
-
-        if (source == butMultiply) {
-            expression = expression + "*";
-        }
-
-        if (source == butDivide) {
-            expression = expression + "/";
-        }
-        if (source == butxpowerofy) {
-            expression = expression + "^";
-        }
-
         if (source == butSquare) {
-            expression = expression + "^2";
+            if(backEnd.getExpression().length()==0||!Character.isDigit(backEnd.getExpression().charAt(backEnd.getExpression().length()-1))){
+                JOptionPane.showMessageDialog(frame, "Cannot square a non-number!");
+                return;
+            }
+            backEnd.addToExpression("^2");
         }
 
-        if (source == butSquareRoot) {
-            expression = expression + "sqrt()";
+        if (source == butExp) {
+            if(backEnd.getExpression().length()==0||!Character.isDigit(backEnd.getExpression().charAt(backEnd.getExpression().length()-1))||backEnd.getExpression().length()==0){
+                JOptionPane.showMessageDialog(frame, "Cannot exponentiate a non-number!");
+                return;
+            }
+            backEnd.addToExpression("^");
         }
 
-        if (source == butOneDevidedBy) {
-            expression = expression + "";
+        if (source == butClear) {
+            backEnd.clear();
         }
 
-        if (source == butCos) {
-            expression = expression + "cos(";
+        if (source == butSqrt) {
+            backEnd.addToExpression("sqrt(");
+        }
+        if (source == butRt) {
+            backEnd.addToExpression("cbrt(");
+        }
+
+        if (source == butLog) {
+            backEnd.addToExpression("log(10,");
+        }
+
+        if (source == butLN) {
+            backEnd.addToExpression("ln");
         }
 
         if (source == butSin) {
-            expression = expression + "sin(";
+            backEnd.addToExpression("sin(");
+        }
+
+        if (source == butCos) {
+            backEnd.addToExpression("cos(");
         }
 
         if (source == butTan) {
-            expression = expression + "tan(";
+            backEnd.addToExpression("tan(");
         }
-        if (source == butlog) {
-            expression = expression + "log";
+        if (source == butDivide) {
+            if(backEnd.getExpression().length()==0||!Character.isDigit(backEnd.getExpression().charAt(backEnd.getExpression().length()-1))||backEnd.getExpression().length()==0){
+                JOptionPane.showMessageDialog(frame, "Syntax: Number must precede division symbol");
+                return;
+            }
+            backEnd.addToExpression("/");
         }
-        if (source == butrate) {
+        if (source == butAdd) {
+            if(backEnd.getExpression().length()==0||!Character.isDigit(backEnd.getExpression().charAt(backEnd.getExpression().length()-1))||backEnd.getExpression().length()==0){
+                JOptionPane.showMessageDialog(frame, "Syntax: Number must precede addition symbol");
+                return;
+            }
+            backEnd.addToExpression("+");
         }
-        if(source == butabs){
-            expression = expression + "abs(";
+        if (source == butMinus) {
+            if(backEnd.getExpression().length()==0||!Character.isDigit(backEnd.getExpression().charAt(backEnd.getExpression().length()-1))||backEnd.getExpression().length()==0){
+                JOptionPane.showMessageDialog(frame, "Syntax: Number must precede minus symbol");
+                return;
+            }
+            backEnd.addToExpression("-");
         }
-
-        if (source == butEqual) {
-            double result = backEnd.evaluate(expression);
-            expression = ""+result;
+        if(source == butMultiply){
+            if(backEnd.getExpression().length()==0||!Character.isDigit(backEnd.getExpression().charAt(backEnd.getExpression().length()-1))||backEnd.getExpression().length()==0){
+                JOptionPane.showMessageDialog(frame, "Syntax: Number must precede multiplication symbol");
+                return;
+            }
+            backEnd.addToExpression("*");
         }
-
-        if (source == butCancel) {
-            expression = "";
+        if (source == butExe) {
+            double result = backEnd.evaluate();
+            backEnd.clear();
+            backEnd.addToExpression(""+result);
         }
-        text.setText(expression);
+        if(source == butEX){
+            backEnd.addToExpression("e^");
+        }
+        if(source == butDot){
+            if(backEnd.getExpression().length()==0||!Character.isDigit(backEnd.getExpression().charAt(backEnd.getExpression().length()-1))||backEnd.getExpression().length()==0){
+                JOptionPane.showMessageDialog(frame, "Syntax: Number must precede a dot");
+                return;
+            }
+            backEnd.addToExpression(".");
+        }
+        if(source == butOpenParen){
+            backEnd.addToExpression("(");
+        }
+        if(source == butCloseParen){
+            backEnd.addToExpression(")");
+        }
+        if(source == butDel){
+            backEnd.del();
+        }
+        if(source == butPi){
+            backEnd.addToExpression("pi");
+        }
+        if(source == butComma){
+            backEnd.addToExpression(",");
+        }
+        if(source == butUndo){
+            backEnd.clear();
+            backEnd.addToExpression(backEnd.getPrevInput());
+        }
+        if(source == butAns){
+            backEnd.addToExpression(""+backEnd.getPrevResult());
+        }
+        text.setText(displayExpression());
        // text.selectAll();
     }
 }
--- src/byRonCalc/calc.java
+++ src/byRonCalc/calc.java
@@ -1,15 +1,58 @@
 package byRonCalc;
 
-import static java.lang.Math.log10;
-import static java.lang.Math.pow;
 import org.mariuszgromada.math.mxparser.*;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class calc {
 
-    public double evaluate(String expression){
+    private String expression;
+    private ArrayList<String> pastInput;
+    private ArrayList<Double> pastResult;
+
+    public calc(){
+        expression = "";
+        pastInput= new ArrayList<>();
+        pastResult = new ArrayList<>();
+    }
+
+    public double evaluate(){
         Expression e = new Expression(expression);
-        return e.calculate();
+        pastInput.add(expression);
+        double result = e.calculate();
+        pastResult.add(result);
+        return result;
+    }
+
+    public double solveEquation(){
+        return 0;
+    }
+
+    public void addToExpression(String in){
+        expression = expression + in;
+    }
+
+    public void clear(){
+        expression = "";
+    }
+    public void del(){
+        if(expression.length()==0){
+            return;
+        }
+        expression = expression.substring(0,expression.length()-1);
+    }
+    public String getExpression(){
+        return expression;
+    }
+
+    public String getPrevInput(){
+        return pastInput.get(pastInput.size()-1);
 
     }
 
+    public double getPrevResult(){
+        return pastResult.get(pastResult.size()-1);
+    }
 }
